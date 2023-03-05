291c4136648cd98b3cdba51838a6c697a492e5ca
Alain Ramon Burkhard <114851831+AlainRBurkhard@users.noreply.github.com>
2023-03-05 15:45:13 +0100

    Add files via upload

---
+++ food_delivery_company/Pages/4_About.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config( page_title= 'About', page_icon = 'ℹ️', layout = 'wide')
+
+
+st.markdown('## About this Project')
+st.markdown(
+"""
+This is a fictional data analysis project and was completed for study purposes. The dataset analyzed was obtained from Kaggle, and I used JupyterLab as my IDE and Python to manipulate and draw insights from the data.
+
+As a first-time project, this experience allowed me to gain practical knowledge and a deeper understanding of the data analysis process. I learned the importance of selecting the right tools and methodologies, asking meaningful research questions, and presenting data effectively. Overall, this project was a valuable learning experience that helped me develop my analytical skills further and explore different techniques used in data analysis.
+
+I'm eager to continue working on similar projects and continue my growth in the field of data analysis.
+
+Thank You! 
+
+Ramon Burkhard
+"""
+)
---
+++ food_delivery_company/functions.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import io
+import plotly.express as px
+import folium
+import streamlit as st
+import matplotlib.pyplot as plt
+from streamlit_folium import folium_static
+from haversine import haversine
+import plotly.graph_objects as go
+import numpy as np
+#====================== Functions ============================#
+
+def clean_df(df):
+    """ 
+    Function responsable for cleaning the dataframe
+    1.delete empty cells on the dataframe 'NaN
+    2.deleting empty spaces in object type cells with strip
+    3.change variables types
+    
+    Input: Dataframe
+    Output: Dataframe
+    """
+
+    # cleaning empty cells#
+
+    df = df[~df.isin(['NaN ']).any(axis=1)].reset_index(drop=True)
+
+    # deleting space#
+    for i in range( len( df ) ):
+        df.loc[i, 'ID'] = df.loc[i, 'ID'].strip()
+        df.loc[i, 'Delivery_person_ID'] = df.loc[i, 'Delivery_person_ID'].strip()
+        df.loc[i, 'Road_traffic_density'] = df.loc[i, 'Road_traffic_density'].strip()
+        df.loc[i, 'Weatherconditions'] = df.loc[i, 'Weatherconditions'].strip()
+    
+    mask = df['Weatherconditions'].str.contains('conditions')
+    # use str.replace() to remove the string 'conditions' from the 'Description' column
+    df.loc[mask, 'Weatherconditions'] = df.loc[mask, 'Weatherconditions'].str.replace('conditions', '')
+
+    cols =['Weatherconditions','Road_traffic_density','Type_of_order','Type_of_vehicle','City']
+    df[cols] = df[cols].apply(lambda x: x.str.strip())
+
+    # transforming variables #
+    df['Delivery_person_Age'] = df['Delivery_person_Age'].astype(int)
+    df['Delivery_person_Ratings'] = df['Delivery_person_Ratings'].astype(float)
+    df['Order_Date'] = pd.to_datetime( df['Order_Date'], format='%d-%m-%Y' )
+    df['multiple_deliveries'] = df['multiple_deliveries'].astype( int )
+    df['Time_taken(min)'] = df['Time_taken(min)'].apply(lambda x: x.split( '(min) ')[1])
+    df['Time_taken(min)'] = df['Time_taken(min)'].astype(int)
+
+    df.reset_index(inplace=True)
+    df.index.astype(int)
+
+    return df
+
+def sidebar(df):
+    
+    
+    st.sidebar.markdown('# iLivery')
+    st.sidebar.markdown('## Fast Home Food')
+    st.sidebar.markdown("""---""")
+
+    ## ---- Date filter ----##
+    st.sidebar.markdown('## Modify Data')
+    date_slider = st.sidebar.slider(
+        'Date',
+        value=pd.datetime(2022,4,13),
+        min_value=pd.datetime(2022,2,11),
+        max_value=pd.datetime(2022,4,6),
+        format='DD-MM-YYYY')
+
+    st.sidebar.markdown("""---""")
+
+    ## ---- Traffic filter ----##
+
+    traffic_conditions = st.sidebar.multiselect(
+        'Traffic density',
+        ['Low', 'Medium', 'High', 'Jam'],
+        default=['Low', 'Medium', 'High', 'Jam'])
+
+    st.sidebar.markdown("""---""")
+
+    ## ---- Weather filter ----##
+
+    Weatherconditions_filter = st.sidebar.multiselect(
+        'Weathercondition',
+        ['Sunny', 'Stormy', 'Cloudy', 'Sandstorms', 'Windy','Fog'],
+        default=['Sunny', 'Stormy', 'Cloudy', 'Sandstorms', 'Windy','Fog'])
+
+    st.sidebar.markdown("""---""")
+    st.sidebar.markdown('#### Powered by Ramon Burkhard')
+
+    #---------setting the filters-------------#
+    #date filter
+    selected_lines = df['Order_Date'] < date_slider
+    df = df.loc[selected_lines,:]
+
+    #traffic filter
+    selected_lines = df['Road_traffic_density'].isin(traffic_conditions)
+    df = df.loc[selected_lines,:]
+
+    # #weathercondition filter
+    selected_lines = df['Weatherconditions'].isin(Weatherconditions_filter)
+    df = df.loc[selected_lines,:]
+    
+    return df
