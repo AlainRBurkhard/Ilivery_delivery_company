93adcf27e4eb70129e72f2f74464ef220b6ac978
Alain Ramon Burkhard <114851831+AlainRBurkhard@users.noreply.github.com>
2023-03-09 20:02:27 +0100

    Update 4_About.py

--- pages/4_About.py
+++ pages/4_About.py
@@ -8,7 +8,7 @@ st.markdown(
 """
 This is a fictional data analysis project and was completed for study purposes. The dataset analyzed was obtained from Kaggle, and I used JupyterLab as my IDE and Python to manipulate and draw insights from the data.
 
-As a first-time project, this experience allowed me to gain practical knowledge and a deeper understanding of the data analysis process. I learned the importance of selecting the right tools and methodologies, asking meaningful research questions, and presenting data effectively. Overall, this project was a valuable learning experience that helped me develop my analytical skills further and explore different techniques used in data analysis.
+As a first-time project on streamlit, this experience allowed me to gain practical knowledge and a deeper understanding of the data analysis process. I learned the importance of selecting the right tools and methodologies, asking meaningful research questions, and presenting data effectively. Overall, this project was a valuable learning experience that helped me develop my analytical skills further and explore different techniques used in data analysis.
 
 I'm eager to continue working on similar projects and continue my growth in the field of data analysis.
 
