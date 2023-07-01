2097e0f3a979fa7b887e92f4b6a0db05e2cce2e3
Alain Ramon Burkhard <114851831+AlainRBurkhard@users.noreply.github.com>
2023-07-01 19:55:46 +0200

    Update 1_Strategy_Panorama.py

--- pages/1_Strategy_Panorama.py
+++ pages/1_Strategy_Panorama.py
@@ -18,10 +18,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from functions import cl_df
-
-
-df = cl_df(df)    
+  
 
 ## ------------------------------------------------##
 #                   STREAMLIT
