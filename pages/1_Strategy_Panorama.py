8f3e746e463bbc2774dbd26ba138758ec5269ae5
Alain Ramon Burkhard <114851831+AlainRBurkhard@users.noreply.github.com>
2023-07-01 19:50:58 +0200

    Update 1_Strategy_Panorama.py

--- pages/1_Strategy_Panorama.py
+++ pages/1_Strategy_Panorama.py
@@ -18,7 +18,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from functions import clean_df
+from functions import cl_df
 
 
 df = cl_df(df)    
